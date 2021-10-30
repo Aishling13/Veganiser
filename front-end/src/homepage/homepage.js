cd99659cbfdb98b949f96796a99a427360494670
DESKTOP-ORF61J3\Tom <casulornstein@gmail.com>
2021-10-30 17:25:34 +0100

    Added submit method

--- front-end/src/homepage/homepage.js
+++ front-end/src/homepage/homepage.js
@@ -4,13 +4,35 @@ import tomato from '../images/tomato.png';
 import pepper from '../images/pepper.png';
 import broccoli from '../images/broccoli.png';
 
+import { useState, useCallback } from 'react'
+
 function HomePage() {
+  const [inputValue, setInputValue] = useState("")
+
+  const handleSubmit = useCallback(() => {
+    const formData = new FormData();
+    formData.append('url', inputValue)
+
+    fetch('http://localhost:5000/veganise', {
+      method: 'POST',
+      body: formData,
+    })
+    .then(res => res.text())
+    .then(body => {
+      document.open();
+      document.write(body);
+      document.close();
+    })
+    .catch(err => alert(err))
+  }, [inputValue])
+
   return (
     <div className="grid-container">
       <h1>Veganise IT!!</h1>
       <div className="content-box">
-        <p>Recipe to Veganise:</p><input/>
-        <button className="submit-button">Submit</button>
+        <p>Recipe to Veganise:</p>
+        <input onChange={e => setInputValue(e.target.value)}/>
+        <button className="submit-button" onClick={() => handleSubmit()}>Submit</button>
       </div>
       <img className="carrot" src={carrot} alt="cartoon carrot"/>
       <img className="tomato" src={tomato} alt="cartoon tomato"/>
